4f4ced80b4a3a42b181f6ce4af122c9a0817f951
shivangraikar <51766272+shivangraikar@users.noreply.github.com>
2020-08-04 01:18:47 +0530

    CRUD operations
    install sqlite3 to connect it with Golang. 
    Follow these steps to run the file-->
    1. Open the command prompt and navigate to the folder where your file is present, 
    e.g. cd desktop\golang
    
    2. Type--> sqlite3 
     To create a database 
     Type--> sqlite3 sutdent.db
    
    3. Run your file --> .read p1.sql

---
+++ p1.sql
@@ -0,0 +1,38 @@
+.open student
+
+drop table if exists student;
+
+create table if not exists student
+(
+id int primary key not null,
+name varchar(100),
+salary double(8,2)
+);
+
+.tables
+
+.schema student
+
+insert into student values(10,'shivang',4500);
+select * from student;
+
+select "************************";
+
+insert into student values(20, 'disha', 3000), (30,'dhruv',4000);
+select * from student;
+select "************************";
+
+update student set salary = salary*1.10
+where id = 10;
+select * from student;
+select "************************";
+
+delete from student where id = 30;
+select * from student;
+select "************************";
+
+replace into student values(30,'dhruv',4000);
+select * from student;
+select "************************";
+
+.quit
